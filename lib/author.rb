60ed7cb8f9540ac33b8ce9d7cb9eeee445e08bc0
Susan Meyers <smmeyers24@gmail.com>
2018-09-28 01:05:25 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,37 @@
+require 'pry'
+class Author
+
+
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+
+  end
+
+  def add_post(post)
+  #binding.pry
+    post.author = self
+    self.posts << post
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    add_post(post)
+    post
+  end
+
+  def self.post_count
+    counter = 0
+    self.each do |post|
+    counter += self.posts.size
+  end
+    counter
+  end
+
+
+
+
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,39 @@
+require 'pry'
+class Post
+  @@all = []
+
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+   @@all
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+
+    else
+      nil
+    end
+  end
+end
+
+
+
+  #
+
+  #
+
+#
+#
+#   end
+#
+#   def self.all?
+#     self.post = []
+#   end
+# end
